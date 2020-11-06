bc41f80c202af23f3315e387861fbdef40a4fe62
Vuong V <3781991+kinekt4@users.noreply.github.com>
2020-11-06 23:57:44 +1100

    Bad urls are added to a tracking file

--- scripts/active-list.sh
+++ scripts/active-list.sh
@@ -9,6 +9,12 @@ cache_dir="${root_dir}/cache"
 adblock_list_file='adblock-list.txt'
 adblock_list_file_path="${root_dir}/${adblock_list_file}"
 
+adblock_bad_file='adblock-bad.txt'
+adblock_bad_file_path="${root_dir}/${adblock_bad_file}"
+
+adblock_tmp_file='adblock-tmp.txt'
+adblock_tmp_file_path="${root_dir}/${adblock_tmp_file}"
+
 i=0
 
 while read line || [ -n "$line" ]; do
@@ -36,17 +42,15 @@ while read line || [ -n "$line" ]; do
     # echo "list_url: ${list_url}"
     echo "${list_url}"
 
-    printf -v j "%02d" $i
-    
-    cache_file="host-list_${j}.txt"
-    cache_file_path="${cache_dir}/${cache_file}"
+    http_code=$(curl -IL --silent $list_url | grep HTTP | tail -n 1 | cut -f 2 -d " ")
 
-    # echo "cache_file_path: ${cache_file_path}"
+    if [ $http_code != "200" ]; then
 
-    wget $list_url -o $cache_file_path
+        # date_iso=$(date +"%Y-%m-%d %H:%M:%S")
+        echo "${http_code}, ${list_url}" >> $adblock_tmp_file_path
 
-    ((i+=1))
-
-    # echo ""
+    fi
     
 done < $adblock_list_file_path
+
+cat $adblock_tmp_file_path | sort | uniq > $adblock_bad_file_path
